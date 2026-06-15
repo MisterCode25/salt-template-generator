e84265a3ab0c3684feb840a7349ce1ee6a30f9df
MisterCode25 <34917835+MisterCode25@users.noreply.github.com>
2026-06-15 18:06:31 +0000

    Deploying to gh-pages from @ MisterCode25/salt-template-generator@673a3fca5820c20a809819dc8d5bd7000c50b141 🚀

--- assets/createLucideIcon-DNgCD5N0.js
+++ assets/createLucideIcon-DNgCD5N0.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-B-Eee9EL.js";/**
+import{r as s}from"./index-COL4ikcm.js";/**
  * @license lucide-react v1.17.0 - ISC
  *
  * This source code is licensed under the ISC license.
